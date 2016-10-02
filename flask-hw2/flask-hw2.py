527744ace7f108e1ec90ba98779429b273e3b53c
Jacob <jacobagraff@gmail.com>
2016-10-02 15:24:27 -0400

    Add password change

--- flask-hw2/flask-hw2.py
+++ flask-hw2/flask-hw2.py
@@ -19,9 +19,42 @@ app.secret_key = '\n\x1f\xe9(\xf0DdG~\xd4\x863\xa0\x10\x1e\xbaF\x10\x16\x7f(\x06
 def hello_world():
     if 'username' not in session:
         return redirect('/login')
-    g.conn = engine.connect()
-    cursor = g.conn.execute('SELECT * FROM userpass WHERE username = %s', session['username'])
-    return cursor.fetchone()[1]
+    return render_template("hello.html", username=session['username'])
+
+@app.route('/change', methods=['GET', 'POST'])
+def change():
+    if request.method == 'POST':
+        if request.form['username'] == '' or\
+                        'username' not in request.form:
+            context = dict(error_message = "No username given")
+            return render_template("change.html", **context)
+        if request.form['password'] == '' or\
+                        'password' not in request.form:
+            context = dict(error_message = "No password given")
+            return render_template("change.html", **context)
+        if request.form['password_conf'] != request.form['new_password'] or\
+                        'new_password' not in request.form:
+            context = dict(error_message = "Passwords must match")
+            return render_template("change.html", **context)
+
+        username = request.form['username'].strip()
+        oldpasshash = hashlib.md5(request.form['password']).hexdigest()
+        newpasshash = hashlib.md5(request.form['new_password']).hexdigest()
+
+        g.conn = engine.connect()
+
+        cursor = g.conn.execute("SELECT * FROM userpass WHERE username=%s", username)
+        hashfound = cursor.first()[1]
+
+        if hashfound == None or hashfound != oldpasshash:
+            context = dict(error_message = "Incorrect username or password")
+            return render_template("change.html", **context)
+
+
+        g.conn.execute("UPDATE userpass SET pass = %s WHERE username = %s", newpasshash, username)
+        return redirect('/')
+
+    return render_template("change.html")
 
 @app.route('/create', methods=['GET', 'POST'])
 def create():
